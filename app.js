c7437b8f2d7735d1e46400374ad21840ed2231af
malberda <malberda15@gmail.com>
2023-04-22 15:21:29 -0600

    asdf

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { Text, View, StyleSheet, TouchableOpacity, TextInput, useState} from 'react-native';
+import { Text, View, StyleSheet, TouchableOpacity, TextInput, useState, Image, Linking, ScrollView, Button} from 'react-native';
 import Constants from 'expo-constants';
 import {SelectDropdown, DropdownData} from "expo-select-dropdown";
 import { SelectList } from 'react-native-dropdown-select-list'
@@ -22,20 +22,49 @@ var hobbySpecific = [
   {key: 3, value: 'Rock Climbing'},
 ]
 var defaultValues = [
-          {key: '1', store: 1, hobbyGeneral: 1, hobbySpecific: 1, link: 'https://www.rei.com/product/193947/arbor-ethos-snowboard-womens-20222023', name:'Arbor Snowboard', price: 239.83},
-          {key: '2', store: 1, hobbyGeneral: 1, hobbySpecific: 2, link: 'https://www.rei.com/product/199291/rossignol-experience-80-carbon-w-skis-with-bindings-womens-20212022', name:'Rossignol Skis', price: 559.83},
-          {key: '3', store: 1, hobbyGeneral: 2, hobbySpecific: 3, link: 'https://www.rei.com/product/203930/la-sportiva-tarantulace-climbing-shoes-mens', name:'Tarantulace Climbing Shoes', price: 89.00},
-          {key: '4', store: 1, hobbyGeneral: 2, hobbySpecific: 3, link: 'https://www.rei.com/product/169549/camp-energy-cr3-harness-mens', name:'C.A.M.P. Energy CR3 Harness', price: 49.95},
+          {key: '1', store: 1, hobbyGeneral: 1, hobbySpecific: 1, link: 'https://www.rei.com/product/193947/arbor-ethos-snowboard-womens-20222023', img: "https://www.rei.com/media/bae1912b-315a-466e-8fff-abcb985a2492.jpg?size=784x588", name:'Arbor Snowboard', price: '239.83'},
+          {key: '2', store: 1, hobbyGeneral: 1, hobbySpecific: 2, link: 'https://www.rei.com/product/199291/rossignol-experience-80-carbon-w-skis-with-bindings-womens-20212022', img:"https://www.rei.com/media/68f7dd36-2e11-49a7-b746-318242191a99.jpg?size=784x588", name:'Rossignol Skis', price: '559.83'},
+          {key: '3', store: 1, hobbyGeneral: 2, hobbySpecific: 3, link: 'https://www.rei.com/product/203930/la-sportiva-tarantulace-climbing-shoes-mens', img:"https://www.rei.com/media/0108620f-59fd-4905-babc-38f98930e187.jpg?size=784x588", name:'Tarantulace Climbing Shoes', price: '89.00'},
+          {key: '4', store: 1, hobbyGeneral: 2, hobbySpecific: 3, link: 'https://www.rei.com/product/169549/camp-energy-cr3-harness-mens', img:"https://www.rei.com/media/109e9e43-259c-4b0e-aafa-51691e4775ba.jpg?size=784x588", name:'C.A.M.P. Energy CR3 Harness', price: '49.95'},
         ]
 
+        
+
 export default function App() {
+  const [selected1, setSelected1] = React.useState('');
+  const [selected2, setSelected2] = React.useState('');
+  const [selected3, setSelected3] = React.useState('');
+
+const buttonPressed = () => {
+  console.log(selected1);
+  console.log(selected2);
+  console.log(selected3);
+}
+
+function handleOpenWithLinking(link) {
+      Linking.openURL(link);
+}
+function shopObjects(values) {
+  console.log()
   return (
-    <View style={styles.container}>
-      {searchSection()}
-      {dropdownStore()}
-      {dropdownHobbyGeneral()}
-      {dropdownHobbySpecific()}
-      {filterSort()}
+    <View>
+      {
+        values.map((val) => {
+          return shopObject(val)
+        })
+      }
+    </View>
+  )
+}
+
+function shopObject(value) {
+  return  (
+    <View>
+      <TouchableOpacity
+              onPress={() => handleOpenWithLinking(value.link)}>
+        <Text>{value.name}: ${value.price}</Text>
+        <Image style={styles.logo} source={{uri: value.img}} />
+      </TouchableOpacity>
     </View>
   );
 }
@@ -68,7 +97,7 @@ function dropdownHobbyGeneral() {
   const data = hobbyGeneral;
 
   return(
-    <SelectList 
+    <SelectList
         setSelected={(val) => setSelected(val)} 
         data={data} 
         save="value"
@@ -103,6 +132,21 @@ function filterSort () {
   )
 }
 
+  return (
+    <ScrollView style={styles.container}>
+      {searchSection()}
+      {dropdownStore()}
+      {dropdownHobbyGeneral()}
+      {dropdownHobbySpecific()}
+      {filterSort()}
+      {shopObjects(defaultValues)}
+      <Button title="asdf" onPress={buttonPressed}>
+      </Button>
+    </ScrollView>
+  );
+
+}// end app()
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
@@ -111,6 +155,10 @@ const styles = StyleSheet.create({
     backgroundColor: '#ecf0f1',
     padding: 8,
     margin: 15,
+  },
+  logo: {
+    width: 200,
+    height: 100
   },
   paragraph: {
     margin: 24,
