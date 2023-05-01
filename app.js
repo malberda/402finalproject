3502831dc5796c2c76eec4185f81b40cee0f7471
malberda <malberda15@gmail.com>
2023-04-30 22:04:19 -0600

    got mostly working brandon did the work

--- app.js
+++ app.js
@@ -14,6 +14,7 @@ import { Card } from 'react-native-paper';
 var stores = [
   {value: 0, label: 'None Selected'},
   {value: 1, label: 'rei.com'},
+  {value: 2, label: 'walmart.com'},
 ]
 var hobbyGeneral = [
   {value: 0, label: 'None Selected'},
@@ -27,8 +28,8 @@ var hobbySpecific = [
   {value: 3, label: 'Rock Climbing'},
 ]
 var defaultValues = [
-          {value: '1', store: 1, hobbyGeneral: 1, hobbySpecific: 1, link: 'https://www.rei.com/product/193947/arbor-ethos-snowboard-womens-20222023', img: "https://www.rei.com/media/bae1912b-315a-466e-8fff-abcb985a2492.jpg?size=784x588", name:'Arbor Snowboard', price: '239.83'},
-          {value: '2', store: 1, hobbyGeneral: 1, hobbySpecific: 2, link: 'https://www.rei.com/product/199291/rossignol-experience-80-carbon-w-skis-with-bindings-womens-20212022', img:"https://www.rei.com/media/68f7dd36-2e11-49a7-b746-318242191a99.jpg?size=784x588", name:'Rossignol Skis', price: '559.83'},
+          {value: '1', store: 2, hobbyGeneral: 1, hobbySpecific: 1, link: 'https://www.rei.com/product/193947/arbor-ethos-snowboard-womens-20222023', img: "https://www.rei.com/media/bae1912b-315a-466e-8fff-abcb985a2492.jpg?size=784x588", name:'Arbor Snowboard', price: '239.83'},
+          {value: '2', store: 2, hobbyGeneral: 1, hobbySpecific: 2, link: 'https://www.rei.com/product/199291/rossignol-experience-80-carbon-w-skis-with-bindings-womens-20212022', img:"https://www.rei.com/media/68f7dd36-2e11-49a7-b746-318242191a99.jpg?size=784x588", name:'Rossignol Skis', price: '559.83'},
           {value: '3', store: 1, hobbyGeneral: 2, hobbySpecific: 3, link: 'https://www.rei.com/product/203930/la-sportiva-tarantulace-climbing-shoes-mens', img:"https://www.rei.com/media/0108620f-59fd-4905-babc-38f98930e187.jpg?size=784x588", name:'Tarantulace Climbing Shoes', price: '89.00'},
           {value: '4', store: 1, hobbyGeneral: 2, hobbySpecific: 3, link: 'https://www.rei.com/product/169549/camp-energy-cr3-harness-mens', img:"https://www.rei.com/media/109e9e43-259c-4b0e-aafa-51691e4775ba.jpg?size=784x588", name:'C.A.M.P. Energy CR3 Harness', price: '49.95'},
         ]
@@ -36,11 +37,12 @@ var defaultValues = [
         
 
 export default function App() {
-  const [selectedOption1, setSelectedOption1] = React.useState("");
-  const [selectedOption2, setSelectedOption2] = React.useState("");
-  const [selectedOption3, setSelectedOption3] = React.useState("");
-  const [selectedOption4, setSelectedOption4] = React.useState("");
+  const [selectedOption1, setSelectedOption1] = React.useState(0);
+  const [selectedOption2, setSelectedOption2] = React.useState(0);
+  const [selectedOption3, setSelectedOption3] = React.useState(0);
+  const [selectedOption4, setSelectedOption4] = React.useState(0);
   const [text, setText] = React.useState('');
+  const [searchResult, setSearchResult] = React.useState(defaultValues);
 
   
   // Handles state change
@@ -48,11 +50,66 @@ export default function App() {
     this.setState({selectValue: event.target.value}, ()=> {alert(`Value: ${this.state.selectValue}`)});
     } 
 
-const buttonPressed = () => {
-  console.log(selectedOption1);
-  console.log(selectedOption2);
-  console.log(selectedOption3);
-  console.log(selectedOption4);
+const buttonPressedSubmit = () => {
+  changeFilterSearch();
+}
+const buttonPressedClear = () => {
+  setText('');
+  setSelectedOption1(0);
+  setSelectedOption2(0);
+  setSelectedOption3(0);
+  setSelectedOption4(0);
+  clearFilterSearch();
+}
+
+function clearFilterSearch() {
+  var newResult = [];
+  defaultValues.forEach((object) => {
+    newResult.push(object);
+  });
+  setSearchResult(newResult);
+}
+
+function changeFilterSearch() {
+  var newResult = [];
+  console.log(defaultValues)
+
+  defaultValues.forEach((object) => {
+    objectName = object.name.toLowerCase();
+    lowerCaseText = text.toLowerCase();
+    if (selectedOption1 == 0 || object.store == selectedOption1) {
+      if (selectedOption2 == 0 || object.hobbyGeneral == selectedOption2) {
+        if (selectedOption3 == 0 || object.hobbySpecific == selectedOption3) {
+          if (objectName.includes(lowerCaseText) || lowerCaseText == "") {
+            newResult.push(object);
+          }
+        }
+      }      
+    } 
+  });
+  if (selectedOption4 == "PriceLowToHigh") {
+    const numAscending = [...newResult].sort((a, b) => a.price - b.price);
+    newResult = numAscending;
+  } else if (selectedOption4 == 'PriceHighToLow') {
+    const numAscending = [...newResult].sort((a, b) => b.price - a.price);
+    newResult = numAscending;
+  }
+  setSearchResult(newResult);
+}
+
+function selectOption1Change (itemValue) {
+  console.log(itemValue);
+  setSelectedOption1(itemValue);
+}
+
+function selectOption2Change (itemValue) {
+  console.log(itemValue);
+  setSelectedOption2(itemValue);
+}
+
+function selectOption3Change (itemValue) {
+  console.log(itemValue);
+  setSelectedOption3(itemValue);
 }
 
 function handleOpenWithLinking(link) {
@@ -100,7 +157,8 @@ function dropdownStore () {
     style={styles.dropDownInput}
     mode={"dialog"}
     selectedValue={selectedOption1}
-    onValueChange={(itemValue) => setSelectedOption1(itemValue)}
+    onValueChange={(itemValue) => selectOption1Change(itemValue)}
+    // onValueChange={(itemValue) => setSelectedOption1(itemValue)}
     
     >
          {stores.map((store, index) => (
@@ -115,7 +173,8 @@ function dropdownHobbyGeneral() {
     style={styles.dropDownInput}
     mode={"dialog"}
     selectedValue={selectedOption2}
-    onValueChange={(itemValue) => setSelectedOption2(itemValue)}
+    onValueChange={(itemValue) => selectOption2Change(itemValue)}
+    //onValueChange={(itemValue) => setSelectedOption2(itemValue)}
     >
          {hobbyGeneral.map((hobby, index) => (
         <Picker.Item label={hobby.label} value={hobby.value} key={hobby.value}/>
@@ -129,7 +188,8 @@ function dropdownHobbySpecific () {
     style={styles.dropDownInput}
     mode={"dialog"}
     selectedValue={selectedOption3}
-    onValueChange={(itemValue) => setSelectedOption3(itemValue)}
+    onValueChange={(itemValue) => selectOption3Change(itemValue)}
+    //onValueChange={(itemValue) => setSelectedOption3(itemValue)}
     > 
         {hobbySpecific.map((hobby, index) => (
         <Picker.Item label={hobby.label} value={hobby.value} key={hobby.value}/>
@@ -165,20 +225,28 @@ function filterSort () {
       {dropdownHobbyGeneral()}
       {dropdownHobbySpecific()}
       {filterSort()}
-      <TouchableOpacity style={styles.btn} onPress={buttonPressed}><Text style={styles.btnText}>Submit Search</Text></TouchableOpacity>
-      {shopObjects(defaultValues)}
+      <View style={{ flexDirection: "row" }}>
+      <TouchableOpacity style={styles.btn} onPress={buttonPressedSubmit}><Text style={styles.btnText}>Submit Search</Text></TouchableOpacity>
+      <TouchableOpacity style={styles.btn} onPress={buttonPressedClear}><Text style={styles.btnText}>Clear Search</Text></TouchableOpacity>
+      </View>
+      {shopObjects(searchResult)}
     </ScrollView>
   );
 
 }// end app()
 
 const styles = StyleSheet.create({
+  dialogContentView: {
+    flex: 1,
+    flexDirection: 'column',
+    justifyContent: 'space-between',
+  },
   dropDownInput: {
     backgroundColor: '#000cb6',
     color: '#00ecdc',
   },
   btn: {
-    width: 230,
+    width: 115,
     alignItems: 'center',
     backgroundColor: '#0005af',
   },
